e3c8d7387a184cba2a4e06789c71474a91b289e3
Deddel <83078233+deddel@users.noreply.github.com>
2024-10-15 11:03:54 +0200

    changed variable names, added argument to bishBosh function, 2 spaces indention

--- bishbosh.js
+++ bishbosh.js
@@ -1,28 +1,26 @@
 
-// Check if a number from 1 to N is divisable by 3, 4, 3 and for.
- 
-function bishBosh(N)
-{
-    let result = [];
-    
-    for (let i = 1; i <= N; i++)
-        {
-        let divByThree = i%3 == 0;
-        let divByFour = i%4 == 0;
-        let divByThreeAndFour = divByThree && divByFour;
-        if (divByThree){
-            console.log('Bish');
+// Check if a number from 1 to N is divisible by 3, 4, 3 and for.
+
+function bishBosh(N, bish, bosh) {
+
+  for (let i = 1; i <= N; i++)
+  {
+      let isDivByBish = i%bish == 0;
+      let isDivByBosh = i%bosh == 0;
+      let isDivByBishAndBosh = isDivByBish && isDivByBosh;
+      if (isDivByBish) {
+        console.log('Bish');
         }
-        else if (divByFour){
-            console.log('Bosh');
+        else if (isDivByBosh) {
+          console.log('Bosh');
         }
-        else if (divByThreeAndFour){
-            console.log('Bish-Bosh');
+        else if (isDivByBishAndBosh) {
+          console.log('Bish-Bosh');
         }
-        else{
-            console.log(i);
-        } 
-    }
+        else {
+          console.log(i);
+        }
+  }
 }
-//N=100
-bishBosh(100);
+
+bishBosh(100, 3, 4);
