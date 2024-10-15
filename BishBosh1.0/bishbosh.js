b72e31a766969aed0907163fa43d14105fefdf64
Deddel <83078233+deddel@users.noreply.github.com>
2024-10-15 22:49:43 +0200

    updated bug in function bishBosh, added input and output to JS

--- BishBosh1.0/bishbosh.js
+++ BishBosh1.0/bishbosh.js
@@ -1,26 +1,27 @@
 
 // Check if a number from 1 to N is divisible by 3, 4, 3 and for.
 
-function bishBosh(N, bish, bosh) {
+function bishBosh(N=100, bish=3, bosh=4) {
 
   for (let i = 1; i <= N; i++)
   {
       let isDivByBish = i%bish == 0;
       let isDivByBosh = i%bosh == 0;
       let isDivByBishAndBosh = isDivByBish && isDivByBosh;
-      if (isDivByBish) {
+      if (isDivByBishAndBosh) {
+        console.log('Bish-Bosh');
+      }
+
+	  else if (isDivByBish) {
         console.log('Bish');
         }
-        else if (isDivByBosh) {
-          console.log('Bosh');
-        }
-        else if (isDivByBishAndBosh) {
-          console.log('Bish-Bosh');
-        }
-        else {
-          console.log(i);
-        }
+      else if (isDivByBosh) {
+        console.log('Bosh');
+      }
+      else {
+        console.log(i);
+    }
   }
 }
 
-bishBosh(100, 3, 4);
+bishBosh();
--- BishBosh2.0/bishbosh.js
+++ BishBosh2.0/bishbosh.js
@@ -8,19 +8,34 @@ function bishBosh(N, bish, bosh) {
       let isDivByBish = i%bish == 0;
       let isDivByBosh = i%bosh == 0;
       let isDivByBishAndBosh = isDivByBish && isDivByBosh;
-      if (isDivByBish) {
-        console.log('Bish');
-        }
-        else if (isDivByBosh) {
-          console.log('Bosh');
-        }
-        else if (isDivByBishAndBosh) {
-          console.log('Bish-Bosh');
-        }
-        else {
-          console.log(i);
-        }
+
+      if (isDivByBishAndBosh) {
+        // console.log('Bish-Bosh');
+        output.innerHTML += `Bish-Bosh  `;
+      }
+      else if (isDivByBish) {
+      // console.log('Bish');
+      output.innerHTML += `Bish  `;
+      }
+      else if (isDivByBosh) {
+        // console.log('Bosh');
+        output.innerHTML += `Bosh  `;
+      }
+      else {
+        // console.log(i);
+        output.innerHTML += i + `  `;
+      }
   }
 }
 
-bishBosh(100, 3, 4);
+
+document.querySelector("#inputForm").addEventListener('submit',function(event){
+  event.preventDefault();
+  const n = document.querySelector('#n').value;
+  const bish = document.querySelector('#bish').value;
+  const bosh = document.querySelector('#bosh').value;
+  output.innerHTML = ''
+  bishBosh(n,bish,bosh)
+
+  // alert("N: " + n + "\nBish: " + bish + "\nBosh: "+ bosh)
+})
